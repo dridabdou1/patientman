ceda8ff8da36682eb8c07d3b9f86aa3994e55407
dridabderrahmane1@gmail.com <numbersami1@gmail.com>
2025-04-01 06:28:31 +0100

    First

---
+++ patientman/src/main/java/com/patientman/patientman/PatientmanApplication.java
@@ -0,0 +1,13 @@
+package com.patientman.patientman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientmanApplication.class, args);
+	}
+
+}
