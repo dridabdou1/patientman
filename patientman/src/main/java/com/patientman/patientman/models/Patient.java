73a9368735d79f60386f20f231b8bde333428a65
dridabderrahmane1@gmail.com <numbersami1@gmail.com>
2025-04-01 10:19:56 +0100

    minor

---
+++ patientman/src/main/java/com/patientman/patientman/models/Patient.java
@@ -0,0 +1,45 @@
+package com.patientman.patientman.models;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;    
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String Email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+    
+    @NotNull
+    private LocalDate RegisterDate;
+}
---
+++ patientman/src/test/java/com/patientman/patientman/PatientmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientman.patientman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
